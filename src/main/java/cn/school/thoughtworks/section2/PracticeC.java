d05eb0afa09302720fc350347235e9385670e066
Pei Zhang <pezhang@thoughtworks.com>
2018-03-08 17:07:45 +0800

    fininsh all answer

--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -1,54 +1,53 @@
 package cn.school.thoughtworks.section2;
 
-import jdk.nashorn.internal.runtime.regexp.joni.Regex;
-
 import java.util.*;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class PracticeC {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        transformOriginCollection(collection1);
+        List<String> list = transformOriginCollection(collection1);
         Set<String> collection1WithoutDuplicates = new HashSet<>(collection1);
         Map<String, Integer> result = new HashMap<>();
         for (String s : collection1WithoutDuplicates) {
-            Integer count = Collections.frequency(collection1, s);
+            Integer count = Collections.frequency(list, s);
             result.put(s, count);
         }
 
         return result;
     }
 
-    private void transformOriginCollection(List<String> collection1) {
+    private List<String> transformOriginCollection(List<String> collection1) {
         List<String> specialElementList = collection1.stream().filter(e -> e.length() > 1).collect(Collectors.toList());
-        Map<String, Integer> result = specialElementList.stream().collect(Collectors.toMap(this::getValue, this::countAmount));
-        flatElementInList(result, collection1);
+        List<String> result = new ArrayList<>(collection1);
+        for (String s : specialElementList) {
+            String value = getValue(s);
+            int count = countAmount(s);
+            pushElementInList(value,count,result);
+        }
+
+        return result;
     }
 
-    private void flatElementInList(Map<String, Integer> result, List<String> collection1) {
-        for (Map.Entry set : result.entrySet()) {
-            int count = (int) set.getValue();
-            for (int i = 0; i < count; i++) {
-                collection1.add((String) set.getKey());
-            }
+    private void pushElementInList(String value, int count, List<String> list) {
+        for (int i = 0; i < count; i++) {
+            list.add(value);
         }
     }
 
     private Integer countAmount(String s) {
-        if (s.contains("-|:")) {
+        if (s.contains("-") || s.contains(":")) {
             return Integer.valueOf(s.split("-|:")[1]);
         }
 
-        return Integer.parseInt(s.charAt(s.indexOf("[") + 1) + "");
+        int index = s.indexOf("[") + 1;
+        return Integer.parseInt(s.substring(index, index + 1));
     }
 
     private String getValue(String s) {
-        if (s.contains("-|:")) {
+        if (s.contains("-") || s.contains(":")) {
             return s.split("-|:")[0];
         }
 
         return s.split("\\[")[0];
     }
-
-
 }
